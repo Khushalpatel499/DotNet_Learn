3cef786157bc05e51a4e0828035df3c01a3317aa
khushalpatel499 <khushalpatel499@gmail.com>
2024-09-25 12:38:05 +0530

    add test1 and test2

---
+++ TestApp1/TestApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+
+     class Program
+    {
+        static void Main(string[] args)
+        {
+        // int empid= 10;
+        //string empname= "Ram";
+        //double empsalary = 1000.8;
+        //Console.WriteLine("Emp id is" +empid);
+        //Console.WriteLine("Emp Name is" +empname);
+        //Console.WriteLine("Emp Salary is" +empsalary);
+        //Console.WriteLine("Welcome to C#");
+
+        int id;
+        string name;
+        double salary;
+
+        Console.WriteLine("Enter the Emp Id:");
+        id = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the Emp Name:");
+        name = Console.ReadLine();
+
+        Console.WriteLine("Enter the Emp Salary:");
+        salary=double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Emp id is" + id);
+        Console.WriteLine("Emp Name is" +name);
+        Console.WriteLine("Emp Salary is" +salary);
+
+        //string interpolation
+        Console.WriteLine($"Emp id is: {id}");
+        Console.ReadKey();//to hold output screen
+        }
+    }
+
---
+++ TestApp2/TestApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Find Emp Salary based on 10% pf
+            string ename;
+            double esalary, pf, netsalary;
+
+            Console.WriteLine("Enter the Emp Name:");
+            ename = Console.ReadLine();
+
+            Console.WriteLine("Enter the Salary:");
+            esalary = double.Parse(Console.ReadLine());
+
+            pf = esalary * 0.1;
+
+            netsalary = esalary - pf;
+            Console.WriteLine($"Emp Name is {ename}");
+            Console.WriteLine($"Emp Basic salary is {esalary}");
+            Console.WriteLine($"Emp pf amount is {pf}");
+            Console.WriteLine($"Emp netsalary is {netsalary}");
+
+            int x=5;
+            long y=x;// Implicit type casting
+            Console.WriteLine($"Value of x is {x} and value of y is{y}");
+            
+            double d=23.5;
+            int f=(int)d;//explicit type casting
+             Console.WriteLine($"Value of d is {d} and value of f is{f}");
+            Console.ReadKey();
+
+
+            
+           
+        }
+    }
+}
